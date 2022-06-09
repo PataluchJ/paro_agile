5ae8b3862ab850132551a760368b9277dfd6cf09
Revek312 <93083745+PataluchJ@users.noreply.github.com>
2022-06-09 19:19:28 +0200

    Added tests for exercise 2

--- exercise2/ut/scrabble-score-tests.cpp
+++ exercise2/ut/scrabble-score-tests.cpp
@@ -1,15 +1,58 @@
 #include "gtest/gtest.h"
 #include "scrabble-score.hpp"
 
-struct ScrabbleScoreTestSuite {};
+std::string word;
 
-TEST(ScrabbleScoreTestSuite, dummyTest)
+TEST(word, EmptyWordGives0)
 {
-  ASSERT_TRUE(false);
+  word = "";
+  ASSERT_EQ(scrabbleScore(word), 0);
 }
 
-TEST(ScrabbleScoreTestSuite, dummyTest2)
+TEST(word, CabbageGives13)
 {
-  ASSERT_EQ(1, 1);
+  word = "CABBAGE";
+  ASSERT_EQ(scrabbleScore(word), 14);
 }
 
+TEST(word, AllOnePointsLettersGive10)
+{
+  word = "AEIOULNRST";
+  ASSERT_EQ(scrabbleScore(word), 10);
+}
+
+TEST(word, AllTwoPointsLettersGive4)
+{
+  word = "DG";
+  ASSERT_EQ(scrabbleScore(word), 4); 
+}
+
+TEST(word, AllThreePointsLettersGive12)
+{
+  word = "BCMP";
+  ASSERT_EQ(scrabbleScore(word), 12); 
+}
+
+TEST(word, AllFourPointsLettersGive16)
+{
+  word = "FHVWY";
+  ASSERT_EQ(scrabbleScore(word), 20); 
+}
+
+TEST(word, AllFivesPointsLettersGive5)
+{
+  word = "K";
+  ASSERT_EQ(scrabbleScore(word), 5); 
+}
+
+TEST(word, AllEightPointsLettersGive16)
+{
+  word = "JX";
+  ASSERT_EQ(scrabbleScore(word), 16); 
+}
+
+TEST(word, AllTenPointsLettersGive20)
+{
+  word = "QZ";
+  ASSERT_EQ(scrabbleScore(word), 20); 
+}
