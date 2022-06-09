af6f21c352877f8b62b713eb2de0c313377a91d3
Revek312 <93083745+PataluchJ@users.noreply.github.com>
2022-06-09 19:37:06 +0200

    Update scrabble-score.cpp

--- exercise2/src/scrabble-score.cpp
+++ exercise2/src/scrabble-score.cpp
@@ -1,26 +1,21 @@
 #include "scrabble-score.hpp"
 #include <map>
 
-std::map<char, int> pointsPerLetter {
-    {'A', 1}, {'E', 1}, {'I', 1}, {'O', 1}, {'U', 1}, {'L', 1}, {'N', 1}, {'R', 1}, {'S', 1}, {'T', 1},
-    {'D', 2}, {'G', 2},
-    {'B', 3}, {'C', 3}, {'M', 3}, {'P', 3}, 
-    {'F', 4}, {'H', 4}, {'V', 4}, {'W', 4}, {'Y', 4}, 
-    {'K', 5}, 
-    {'J', 8}, {'X', 8}, 
-    {'Q', 10}, {'Z', 10}, 
+
+std::map<char, int> pointsPerLetter{
+    {'A', 1}, {'E', 1}, {'I', 1}, {'O', 1},  {'U', 1},  {'L', 1}, {'N', 1},
+    {'R', 1}, {'S', 1}, {'T', 1}, {'D', 2},  {'G', 2},  {'B', 3}, {'C', 3},
+    {'M', 3}, {'P', 3}, {'F', 4}, {'H', 4},  {'V', 4},  {'W', 4}, {'Y', 4},
+    {'K', 5}, {'J', 8}, {'X', 8}, {'Q', 10}, {'Z', 10},
 };
 
 int scrabbleScore(std::string wordToScore) {
-    int score = 0;
+    int score        = 0;
     std::string test = "A";
-    for(int i = 0; i < wordToScore.size(); i++)
-    {
-        if(pointsPerLetter.count(wordToScore[i]) == 1)
-        {
+    for(int i = 0; i < wordToScore.size(); i++) {
+        if(pointsPerLetter.count(wordToScore[i]) == 1) {
             score += pointsPerLetter[wordToScore[i]];
         }
     }
     return score;
 }
-
