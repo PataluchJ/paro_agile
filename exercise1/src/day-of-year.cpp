ac3eff65555af41b5ddbf35b034e74ba9fc0607d
Kamil <keimil00@gmail.com>
2022-06-09 19:24:26 +0200

    "First user story."

--- exercise1/src/day-of-year.cpp
+++ exercise1/src/day-of-year.cpp
@@ -2,7 +2,7 @@
 
 int dayOfYear(int month, int dayOfMonth, int year) {
     int feb = 28;
-    if ((year % 4 == 0 and year % 100 != 0) or year % 400 == 0) {
+    if((year % 4 == 0 and year % 100 != 0) or year % 400 == 0) {
         feb = 29;
     }
     int months[12] = {31, feb, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
