900d89e125b3b5bc319d982db32a4cd00b86e0b9
Jędrzej Urbaniak <253235@student.pwr.edu.pl>
2022-06-09 19:05:37 +0200

    Added exercise2

---
+++ exercise2/ut/scrabble-score-tests.cpp
@@ -0,0 +1,15 @@
+#include "gtest/gtest.h"
+#include "scrabble-score.hpp"
+
+struct ScrabbleScoreTestSuite {};
+
+TEST(ScrabbleScoreTestSuite, dummyTest)
+{
+  ASSERT_TRUE(false);
+}
+
+TEST(ScrabbleScoreTestSuite, dummyTest2)
+{
+  ASSERT_EQ(1, 1);
+}
+
